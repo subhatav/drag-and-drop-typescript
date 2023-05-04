f82b67364d399f2cff0a3e0d0bbfbee15912f7c4
Subhatav Dhali <subhatav@gmail.com>
2023-05-04 15:04:03 +0530

    Implement the logic for validations

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,121 @@
-function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+interface ValidatableInput {
+  input: HTMLInputElement;
+
+  name: string;
+  value: string | number;
+
+  needed?: boolean;
+
+  minLength?: number;
+  maxLength?: number;
+
+  minValue?: number;
+  maxValue?: number;
+}
+
+function validateInput(submittedInput: ValidatableInput): [boolean, string[]] {
+  let isValidInput = true;
+  let validationErrors: string[] = [];
+
+  const inputName = submittedInput.name;
+  const inputValue = submittedInput.value;
+
+  const minimumLen = submittedInput.minLength;
+  const maximumLen = submittedInput.maxLength;
+
+  const minimumVal = submittedInput.minValue;
+  const maximumVal = submittedInput.maxValue;
+
+  if (submittedInput.needed) {
+    const trimmedValue = inputValue.toString().trim();
+    const isPresent = trimmedValue.length !== 0;
+
+    isValidInput = isValidInput && isPresent;
+
+    if (!isPresent) {
+      validationErrors.push(
+        `Field of <${inputName}> cannot be empty / must be present`
+      );
+    }
+  }
+
+  if (minimumLen != null && typeof inputValue === "string") {
+    const isLengthAboveMinimum = inputValue.length >= minimumLen;
+
+    isValidInput = isValidInput && isLengthAboveMinimum;
+
+    if (!isLengthAboveMinimum) {
+      validationErrors.push(
+        `Length of <${inputName}> must be equal to or greater than ${minimumLen}`
+      );
+    }
+  }
+
+  if (maximumLen != null && typeof inputValue === "string") {
+    const isLengthBelowMaximum = inputValue.length <= maximumLen;
+
+    isValidInput = isValidInput && isLengthBelowMaximum;
+
+    if (!isLengthBelowMaximum) {
+      validationErrors.push(
+        `Length of <${inputName}> must be equal to or lesser than ${maximumLen}`
+      );
+    }
+  }
+
+  if (minimumVal != null && typeof inputValue === "number") {
+    const isValueGreaterThanMinimum = inputValue >= minimumVal;
+
+    isValidInput = isValidInput && isValueGreaterThanMinimum;
+
+    if (!isValueGreaterThanMinimum) {
+      validationErrors.push(
+        `Value of <${inputName}> must be equal to or greater than ${minimumVal}`
+      );
+    }
+  }
+
+  if (maximumVal != null && typeof inputValue === "number") {
+    const isValueLesserThanMaximum = inputValue <= maximumVal;
+
+    isValidInput = isValidInput && isValueLesserThanMaximum;
+
+    if (!isValueLesserThanMaximum) {
+      validationErrors.push(
+        `Value of <${inputName}> must be equal to or lesser than ${maximumVal}`
+      );
+    }
+  }
+
+  return [isValidInput, validationErrors];
+}
+
+function showCustomAlert(errorMessages: string[]) {
+  let customMessage = "";
+
+  if (errorMessages.length > 0) {
+    customMessage = `Invalid input! Please try again. Errors: \n${errorMessages.join(
+      "; and, \n"
+    )}.`;
+  }
+
+  if (customMessage.length > 0) {
+    alert(customMessage);
+  }
+}
+
+function customFocus(validatableInputs: ValidatableInput[]): void {
+  for (const validatableInput of validatableInputs) {
+    const [isValid, _]: [boolean, string[]] = validateInput(validatableInput);
+
+    if (!isValid) {
+      validatableInput.input.focus();
+      break;
+    }
+  }
+}
+
+function AutoBind(_1: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
 
   const adjustedDescriptor: PropertyDescriptor = {
@@ -19,14 +136,17 @@ class ProjectInput {
 
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
-  peopleInputElement: HTMLInputElement;
+  peopleCountInputElement: HTMLInputElement;
 
   constructor() {
+    // `templateElement` gives access to the Template that holds this Template Content
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
+    // `hostElement` holds a Reference to the Element where Template Content will be rendered
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
+    // Deep Cloning => all the Levels of Nesting inside the Template
     const importedNode = document.importNode(
       this.templateElement.content,
       true
@@ -41,39 +161,77 @@ class ProjectInput {
     this.descriptionInputElement = this.formElement.querySelector(
       "#description"
     )! as HTMLInputElement;
-    this.peopleInputElement = this.formElement.querySelector(
+    this.peopleCountInputElement = this.formElement.querySelector(
       "#people"
     )! as HTMLInputElement;
 
     this.configure();
-    this.attach();
+    this.attach(this.formElement);
   }
 
   private gatherUserInput(): [string, string, number] | void {
-    const enteredTitle = this.titleInputElement.value;
-    const enteredDescription = this.descriptionInputElement.value;
-    const enteredPeople = this.peopleInputElement.value;
+    const enteredTitle: string = this.titleInputElement.value;
+    const enteredDescription: string = this.descriptionInputElement.value;
+    const enteredPeopleCount: number = +this.peopleCountInputElement.value;
 
-    if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
-    ) {
-      alert("Invalid input values! Please try again.");
+    const titleValidatable: ValidatableInput = {
+      input: this.titleInputElement,
+      name: "Title",
+      value: enteredTitle,
+      needed: true,
+    };
+
+    const descriptionValidatable: ValidatableInput = {
+      input: this.descriptionInputElement,
+      name: "Description",
+      value: enteredDescription,
+      needed: true,
+      minLength: 5,
+    };
+
+    const peopleCountValidatable: ValidatableInput = {
+      input: this.peopleCountInputElement,
+      name: "People",
+      value: +enteredPeopleCount,
+      needed: true,
+      minValue: 4,
+      maxValue: 7,
+    };
+
+    const [isTitleValid, titleErrors]: [boolean, string[]] =
+      validateInput(titleValidatable);
+    const [isDescriptionValid, descriptionErrors]: [boolean, string[]] =
+      validateInput(descriptionValidatable);
+    const [isPeopleCountValid, peopleErrors]: [boolean, string[]] =
+      validateInput(peopleCountValidatable);
+
+    const isValidForm =
+      isTitleValid && isDescriptionValid && isPeopleCountValid;
+
+    if (!isValidForm) {
+      // Shows a proper Custom Alert
+      showCustomAlert(
+        titleErrors.concat(descriptionErrors).concat(peopleErrors)
+      );
+
+      // Focus on the first Input with issues
+      customFocus([
+        titleValidatable,
+        descriptionValidatable,
+        peopleCountValidatable,
+      ]);
+
+      // If the function includes a `void` as possible output,
+      // then the returned `undefined` value will be ignored
       return;
     } else {
-      return [enteredTitle, enteredDescription, +enteredPeople];
+      return [enteredTitle, enteredDescription, enteredPeopleCount];
     }
   }
 
-  private clearInputs() {
-    this.titleInputElement.value = "";
-    this.descriptionInputElement.value = "";
-    this.peopleInputElement.value = "";
-  }
-
-  @autobind
+  @AutoBind
   private submitHandler(event: Event) {
+    // Prevents the default HTTP Request
     event.preventDefault();
 
     const userInput = this.gatherUserInput();
@@ -85,13 +243,15 @@ class ProjectInput {
     }
   }
 
+  private clearInputs = () => this.formElement.reset();
+
   private configure() {
     this.formElement.addEventListener("submit", this.submitHandler);
   }
 
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  private attach(fragment: Element) {
+    this.hostElement.insertAdjacentElement("afterbegin", fragment);
   }
 }
 
-const prjInput = new ProjectInput();
+const myProject = new ProjectInput();
